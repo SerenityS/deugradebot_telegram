40e567af8d5efd0f0b5118d3490c4f91e1d67bfc
HeeRyun, Jin <jins4218@gmail.com>
2021-12-21 00:25:37 +0900

    Update Login Function

--- bot.py
+++ bot.py
@@ -11,11 +11,19 @@ def login_deu():
     login_result = s.post(app_login_url, json=login_data)
 
     if login_result.json()['SUCCESS_YN'] == "Y":
+        login_deu_web()
         print("로그인 성공")
     else:
         print("로그인 실패")
         raise Exception("LoginInfoMismatch")
 
 
+def login_deu_web():
+    web_login_url = "https://smartdeu.deu.ac.kr/weblogin.do"
+    login_data = {'user_id': id, 'passwd': pw}
+
+    s.post(web_login_url, data=login_data)
+
+
 if __name__ == "__main__":
-    login_deu()
+    login_deu_app()
